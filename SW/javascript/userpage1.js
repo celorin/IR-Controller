40eb12ecd808566d3ce4f14adb733830f0975526
celorin <terminus1192@gmail.com>
2019-06-28 08:47:57 +0900

    user_page edited

--- SW/javascript/userpage1.js
+++ SW/javascript/userpage1.js
@@ -18,4 +18,13 @@ $(function(){
       $('nav').removeClass('open');      
     }
   });
+  
+  $(".btn1").on("click", function(){
+    $("#overlay").fadeIn(500);
+    setTimeout(function(){
+        $("#overlay").fadeOut(500);
+        $("form").submit();
+    },3000);
+  });
+  
 });
