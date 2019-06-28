05346b9146a735546851f2e7ab951254d2d543c9
fiji <ikuy.s.fw11@gmail.com>
2019-06-28 13:42:02 +0900

    complete

--- SW/javascript/support.js
+++ SW/javascript/support.js
@@ -1,24 +1,4 @@
 
-$(function(){
-    $('.menu-trigger').on('click',function(){
-      if($(this).hasClass('active')){
-        $(this).removeClass('active');
-        $('nav').removeClass('open');
-        $('.overlay').removeClass('open');
-      } else {
-        $(this).addClass('active');
-        $('nav').addClass('open');
-        $('.overlay').addClass('open');
-      }
-    });
-    $('.overlay').on('click',function(){
-      if($(this).hasClass('open')){
-        $(this).removeClass('open');
-        $('.menu-trigger').removeClass('active');
-        $('nav').removeClass('open');      
-      }
-    });
-});
 
 function check(){
   if(window.confirm('送信してよろしいですか？')){ 
--- SW/javascript/userpage1.js
+++ SW/javascript/userpage1.js
@@ -1,24 +1,6 @@
 
 $(function(){
-  $('.menu-trigger').on('click',function(){
-    if($(this).hasClass('active')){
-      $(this).removeClass('active');
-      $('nav').removeClass('open');
-      $('.overlay').removeClass('open');
-    } else {
-      $(this).addClass('active');
-      $('nav').addClass('open');
-      $('.overlay').addClass('open');
-    }
-  });
-  $('.overlay').on('click',function(){
-    if($(this).hasClass('open')){
-      $(this).removeClass('open');
-      $('.menu-trigger').removeClass('active');
-      $('nav').removeClass('open');      
-    }
-  });
-  
+
   $(".btn1").on("click", function(){
     $("#overlay").fadeIn(500);
     setTimeout(function(){
